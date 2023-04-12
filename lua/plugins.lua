bb67f79391a967aeb19c685c7ff30bad828f9f1b
xavier-kong <kongxianchengxavier@gmail.com>
2023-04-12 17:02:22 +0800

    added ts utils

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -167,7 +167,8 @@ require("lazy").setup({
         event = {"CmdlineEnter"},
         ft = {"go", 'gomod'},
         build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-    }
+    },
+    "jose-elias-alvarez/nvim-lsp-ts-utils"
 })
 
 require('impatient')
