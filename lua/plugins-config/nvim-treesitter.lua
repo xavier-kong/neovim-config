336e95d24d0c64bed9aa4640826d11668b582590
xavier-kong <kongxianchengxavier@gmail.com>
2024-02-06 22:01:16 +0800

    so somehow syntax highlighting was not enabled previously but it still highlighted!

--- lua/plugins-config/nvim-treesitter.lua
+++ lua/plugins-config/nvim-treesitter.lua
@@ -2,6 +2,7 @@ require('nvim-treesitter.configs').setup({
         ensure_installed = { 'html', 'json', 'typescript', 'javascript', 'lua', 'c', 'rust', 'go', "toml", 'css', 'astro', 'tsx' },
         highlight = {
                 enabled = true,
+                enable = true
         },
         autotag = {
                 enable = true
